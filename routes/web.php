f64fc0bf0c217bf1fc023ba8f6b9d5599545a8d1
adsengila-collab <adsengila@gmail.com>
2026-04-15 12:28:24 +0700

    Create routes/web.php file with example Laravel routes for Inertia React setup

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return inertia('Home');
+});
+
+Route::get('/about', function () {
+    return inertia('About');
+});
+
+Route::get('/contact', function () {
+    return inertia('Contact');
+});
